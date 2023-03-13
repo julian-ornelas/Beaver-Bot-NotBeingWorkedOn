8ef01e56a8fcf67f8a3fc361f1e75c082bd5ab9b
Julian Ornelas <julian_ornelas45@protonmail.com>
2023-03-13 15:39:46 -0400

    Added sample slash command.

--- bot.py
+++ bot.py
@@ -8,17 +8,15 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents, activity=discord.Game(name="In Production"))
+tree = discord.app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
+    await tree.sync()
     print(f"We have logged in as {client.user}")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@tree.command(name="ping", description="test command")
+async def ping(message):
+    await message.response.send_message("pong")
 
 client.run(os.getenv("discord_token"))
