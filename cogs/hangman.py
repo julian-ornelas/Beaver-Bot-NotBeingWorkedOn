50ace17a8cb5c0f212d2149d09dcc996a3f7c18e
Julian Ornelas <julian_ornelas45@protonmail.com>
2023-03-15 21:42:26 -0400

    Need to add user interaction, continue hangman

--- cogs/hangman.py
+++ cogs/hangman.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import random
+import requests
+import json
 
 class hangman(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -18,7 +21,32 @@ class hangman(commands.Cog):
         discord.app_commands.Choice(name="long", value=3),
     ])
     async def hangman(self, interaction: discord.Interaction, length: discord.app_commands.Choice[int]):
+        
         await interaction.response.send_message(f"you have chosen {length.name}")
+        game_length = length.value
+
+        #code from my hangman repo
+        if game_length == 1:
+            number = str(random.randint(4, 6))
+            word_request = requests.get("https://random-word-api.herokuapp.com/word?length=" + number)
+        elif game_length == 2:
+            number = str(random.randint(7, 10))
+            word_request = requests.get("https://random-word-api.herokuapp.com/word?length=" + number)
+        elif game_length == 3:
+            number = str(random.randint(11, 15))
+            word_request = requests.get("https://random-word-api.herokuapp.com/word?length=" + number)
+        
+        word = json.loads(word_request.text)
+        word = json.dumps(word)
+        word = word[2:-2]
+
+        decompsed_word = list(word)
+
+        word_length = len(word)
+
+        #need to figure out embeds
+        print(word)
+        print()
 
 async def setup(bot):
     await bot.add_cog(hangman(bot))
